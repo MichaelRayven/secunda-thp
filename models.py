187ffc26b5d558e3775e65773ff076668a8b6d46
MichaelRayven <themikerayven@gmail.com>
2025-11-01 18:42:22 +0700

    fix: deleted unused models

--- models.py
+++ models.py
@@ -69,28 +69,3 @@ class Organization(Base):
         secondary=organization_activity,
         backref='organizations',
     )
-
-
-class User(Base):
-    __tablename__ = 'user_account'
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(30))
-    fullname: Mapped[str | None]
-    addresses: Mapped[list['Address']] = relationship(
-        back_populates='user',
-        cascade='all, delete-orphan',
-    )
-
-    def __repr__(self) -> str:
-        return f'User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})'
-
-
-class Address(Base):
-    __tablename__ = 'address'
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email_address: Mapped[str]
-    user_id: Mapped[int] = mapped_column(ForeignKey('user_account.id'))
-    user: Mapped['User'] = relationship(back_populates='addresses')
-
-    def __repr__(self) -> str:
-        return f'Address(id={self.id!r}, email_address={self.email_address!r})'
