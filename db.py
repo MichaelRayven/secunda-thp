8ee83b6ec4d9458d7871fd369e8b261f0b420d82
MichaelRayven <themikerayven@gmail.com>
2025-11-01 18:44:21 +0700

    feat: add database session dependency type for fastapi

--- db.py
+++ db.py
@@ -1,6 +1,9 @@
 import os
 
+from fastapi import Depends
+from pydantic import Annotated
 from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 engine = create_engine(
     os.environ.get('DATABASE_URL'),
@@ -8,3 +11,11 @@ engine = create_engine(
         'isolation_level': 'REPEATABLE READ',
     },
 )
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
